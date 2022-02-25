1d6d032b2de786ced5da923d1154e84a2adf30f5
MiguelE8a <m.e.o.a2000@gmail.com>
2022-02-25 17:22:35 -0300

    en este comit trate de acomodar el menu del phoce cel, ahi vamos

--- menu.js
+++ menu.js
@@ -1,17 +1,33 @@
-const iconoMenu = document.querySelector('#icono-menu'),
-    menu = document.querySelector('#menu')
+// const iconoMenu = document.querySelector('#icono-menu'),
+//     menu = document.querySelector('#menu')
 
-iconoMenu.addEventListener('click', (e) =>{
+// iconoMenu.addEventListener('click', (e) =>{
 
-    // alternamos estilos para el menu y body
-    menu.classList.toggle('active')
-    document.body.classList.toggle('opacity')
+//     // alternamos estilos para el menu y body
+//     menu.classList.toggle('active')
+//     document.body.classList.toggle('opacity')
 
-    const rutaActual = e.target.getAttribute('src')
+//     const rutaActual = e.target.getAttribute('src')
 
-    if(rutaActual == "./images/menu.png"){
-        e.target.setAttribute('src', './images/menu2.png')
-    }else{
-        e.target.setAttribute('src', './images/menu.png')
-    }
+//     if(rutaActual == "./images/menu.png"){
+//         e.target.setAttribute('src', './images/menu2.png')
+//     }else{
+//         e.target.setAttribute('src', './images/menu.png')
+//     }
+// })
+
+// const menu = document.querySelector('.menu')
+// const checkbox = document.querySelector('#btn-menu')
+// const btnClosed = document.querySelector('.btn-closed')
+
+// btnClosed.addEventListener('click', (e)=>{
+
+//     btnClosed.style.opacity = '0'
+//     menu.style.left = '-300px'
+//     console.log('dale')
+
+    // const rutaActual = e.target.classList
+    // if(e.classList == 'checked'){
+    //     console.log('des')
+    // }
 })
